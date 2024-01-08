7528a15a4a747bf91b2d28dadeaa9f5ec7f25a31
Владимир <@KOMPUTER>
2024-01-08 17:40:41 +0200

    Добавьте файлы проекта.

---
+++ Дз Цифра C# №2/Program.cs
@@ -0,0 +1,4 @@
+﻿using System.Diagnostics;
+using System.Linq;
+
+Console.WriteLine("Two roads diverged in a yellow wood,\nAnd sorry I could not travel both \nAnd be one traveler, long I stood \nAnd looked down one as far as I could\nTo where it bent in the undergrowth");
