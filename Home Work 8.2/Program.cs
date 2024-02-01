9aa81ee9c620be3f7bd573a472b6d415f6bf718d
DawidWilson <86928107+DawidWilson@users.noreply.github.com>
2024-02-02 01:37:23 +0200

    HomeWork 8.2

---
+++ Home Work 8.2/Program.cs
@@ -0,0 +1,14 @@
+﻿//8.2.1
+int momByAggs = 13;
+int whenEatLeftAggs = 9;
+Console.WriteLine($"Всего съели {momByAggs-whenEatLeftAggs} яиц");
+
+//8.2.2
+double magazineWidth = 25;
+Console.WriteLine($"Ширина газеты {magazineWidth + 16}см");
+
+// 8.2.3
+double potato = 44;
+double beet = 44/4;
+double onion = beet/2;
+Console.WriteLine($"Повар потратил {onion}кг лука");
