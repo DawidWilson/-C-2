063d34a1acabe83400bc0d6b0e4541dafbde5179
DawidWilson <86928107+DawidWilson@users.noreply.github.com>
2024-02-14 00:16:04 +0200

    Home Work 9.2

---
+++ HomeWork 10.1/Program.cs
@@ -0,0 +1,11 @@
+﻿// ДЗ 10.1
+Console.WriteLine("Введите значения радиуса окружности: ");
+double R = double.Parse(Console.ReadLine());
+double S = Math.Pow (Math.PI * R, 2);
+double L = 2 * Math.PI * R;
+Console.WriteLine($"Длина окружности {L}\nПлощадь окружности {S}");
+//ДЗ 10.2
+Console.WriteLine("Введите значение катета и гипатенузы");
+double catet = double.Parse(Console.ReadLine());
+double gipaten = double.Parse(Console.ReadLine());
+double catet2 = (Math.Pow(catet, 2) - Math.Pow(gipaten,2));
