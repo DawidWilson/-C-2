84e0fe6d576b7739e246e037bb039901763a5cee
Владимир <Sovasol19@gmail.com>
2024-02-27 23:21:36 +0300

    HomeWork 11.1

--- HomeWork 10.1/Program.cs
+++ HomeWork 10.1/Program.cs
@@ -5,8 +5,8 @@
 //double L = 2 * Math.PI * R;
 //Console.WriteLine($"Длина окружности {L}\nПлощадь окружности {S}");
 //ДЗ 10.2
-Console.WriteLine("Введите значение катета и гипатенузы");
-double catet = double.Parse(Console.ReadLine());
-double gipaten = double.Parse(Console.ReadLine());
-double catet2 = Math.Sqrt((gipaten*gipaten)-(catet*catet)); // а2+б2=с2
-Console.WriteLine($"{catet2:F2}") ;
+//Console.WriteLine("Введите значение катета и гипатенузы");
+//double catet = double.Parse(Console.ReadLine());
+//double gipaten = double.Parse(Console.ReadLine());
+//double catet2 = Math.Sqrt((gipaten*gipaten)-(catet*catet)); // а2+б2=с2
+//Console.WriteLine($"{catet2:F2}") ;
