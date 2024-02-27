429fa9b0070a143aa86c6084b78facd9eb4bc8e4
Владимир <Sovasol19@gmail.com>
2024-02-27 23:58:04 +0300

    Home Work 11.2

---
+++ Home Work 11.2/Program.cs
@@ -0,0 +1,8 @@
+﻿// Тема 11 задание 2
+
+Console.Write("Укажите сколько сейчас градусов на улице: ");
+int Temperature = int .Parse(Console.ReadLine());
+
+if (Temperature > 20) Console.WriteLine(" Сегодня можно выпустить слонов, жирафов и львов");
+else if (Temperature < 20) Console.WriteLine("Сегодня можно выпустить медведей,волков и пингвинов");
+else Console.WriteLine("Сегодня можно выпустить  только кенгуру ");
