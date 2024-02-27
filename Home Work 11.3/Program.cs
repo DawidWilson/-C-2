dcfeb05276144f55cbdbaca40f457e832a0ac2d7
Владимир <Sovasol19@gmail.com>
2024-02-28 00:39:42 +0300

    Home Work 11.3

--- Home Work 11.3/Program.cs
+++ Home Work 11.3/Program.cs
@@ -1,11 +1,39 @@
-﻿                    // Тема 11 Задание 3 
+﻿// Тема 11 Задание 3 
+// №1
+//Random random = new Random();
+//Console.Write("Введите число от 0 до 5: ");
+//int numb = int.Parse(Console.ReadLine());
+//int a = random.Next(0, 5);
 
-Random random = new Random();
-Console.Write("Введите число от 0 до 5: ");
-int numb = int.Parse(Console.ReadLine());
-int a = random.Next(0, 5);
+//if (numb > a) Console.WriteLine("Поражение :(");
+//else if (numb < a) Console.WriteLine("Поражение :( ");
+//else Console.WriteLine("Вы выйграли :) ");
 
-if (numb > a) Console.WriteLine("Поражение :(");
-else if (numb < a) Console.WriteLine("Поражение :( ");
-else Console.WriteLine("Вы выйграли :) ");
+// №2
+
+//Console.Write("Введите порядковый номер текущего месяца: ");
+//int mothNumber = int.Parse(Console.ReadLine());
+
+//Console.Write("Введите среднюю температуру: ");
+//int temperature = int.Parse(Console.ReadLine());
+
+
+//if (temperature > 0)
+//{
+//    if (mothNumber == 1)Console.WriteLine("Январь");
+//    if (mothNumber == 2) Console.WriteLine("Февраль");
+//    if (mothNumber == 11) Console.WriteLine("Ноябрь");
+//    if (mothNumber == 12) Console.WriteLine("Декабрь");
+//    else Console.WriteLine("Дождливая зима");
+
+//}
+//if (mothNumber == 3) Console.WriteLine("Март");
+//if (mothNumber == 4) Console.WriteLine("Апрель");
+//if (mothNumber == 5) Console.WriteLine("Май");
+//if (mothNumber == 6) Console.WriteLine("Июнь");
+//if (mothNumber == 7) Console.WriteLine("Июль");
+//if (mothNumber == 8) Console.WriteLine("Август");
+//if (mothNumber == 9) Console.WriteLine("Сентябрь");
+//if (mothNumber == 10) Console.WriteLine("Октябрь");
+
 
