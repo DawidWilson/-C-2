d927c118643ab82f96122085aff7cb70db620704
DawidWilson <86928107+DawidWilson@users.noreply.github.com>
2024-02-28 00:12:52 +0300

    Revert "Home work"

--- Дз Цифра C# №2/Program.cs
+++ Дз Цифра C# №2/Program.cs
@@ -1,11 +1,4 @@
-﻿Console.WriteLine("Two roads diverged in a yellow wood,\nAnd sorry I could not travel both \nAnd be one traveler, long I stood \nAnd looked down one as far as I could\nTo where it bent in the undergrowth");
+﻿using System.Diagnostics;
+using System.Linq;
 
-Console.WriteLine($" {1} {13} {49}");
-
-Console.Write("Введите первое число ");
-int a = int.Parse(Console.ReadLine());
-Console.Write("Введите второе число ");
-int b = int.Parse(Console.ReadLine());
-Console.Write("Введите третье число ");
-int c = int.Parse(Console.ReadLine());
-Console.WriteLine($"Вы ввели число {a}  {b}  {c}");
+Console.WriteLine("Two roads diverged in a yellow wood,\nAnd sorry I could not travel both \nAnd be one traveler, long I stood \nAnd looked down one as far as I could\nTo where it bent in the undergrowth");
