e95d37548de07901375ac8a0bb903b151f2531e7
Владимир <Sovasol19@gmail.com>
2024-03-01 00:42:40 +0300

    HomeWork 12.1

---
+++ Home Work 12.1/Program.cs
@@ -0,0 +1,17 @@
+﻿
+Console.Write("Введите первое число: ");
+int number1 = int.Parse(Console.ReadLine());
+Console.Write("Введите второе число: ");
+int number2 = int.Parse(Console.ReadLine());
+Console.Write("Введите операцию: ");
+char operation = char.Parse(Console.ReadLine());
+
+double total = 0;
+
+switch (total)
+{
+    case 0: { total = number1 + number2; Console.WriteLine($"{number1}{operation}{number2}={total}"); } break;
+    case 1: { total = number1 - number2; Console.WriteLine($"{number1}{operation}{number2}={total}"); } break;
+    case 2: { total = number1 / number2; Console.WriteLine($"{number1}{operation}{number2}={total}"); } break;
+    case 3: { total = number1 * number2; Console.WriteLine($"{number1}{operation}{number2}={total}"); } break;
+}
