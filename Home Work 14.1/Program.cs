fe4bb80c2d8dac84635f83823ff0bfc4847aa65d
Владимир <Sovasol19@gmail.com>
2024-03-06 18:46:36 +0300

    Home Work 14.1

---
+++ Home Work 14.1/Program.cs
@@ -0,0 +1,10 @@
+﻿Console.Write("Введите длину пароля: ");
+int longPas = int .Parse(Console.ReadLine());
+Random random = new Random();
+int password = 0;
+
+for (int i = 0; i < longPas; i++)
+{
+    password = random.Next(65, 115);
+    Console.Write($"{(char)password}");
+}
