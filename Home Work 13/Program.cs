a7a96c127e78b6f3b8e5d09683b93ec7736b431a
DawidWilson <86928107+DawidWilson@users.noreply.github.com>
2024-03-06 16:56:03 +0200

    Home Work 13.1

---
+++ Home Work 13/Program.cs
@@ -0,0 +1,24 @@
+﻿Random random = new Random();
+int aiNumber = random.Next(1,6);
+int user;
+int i = 3;
+do
+{
+    i--;
+    Console.Write("Введите число от 1 до 5: ");
+    user = int.Parse(Console.ReadLine());
+    if (i < 3)
+    {
+        Console.WriteLine($"Попыток осталось {i}");
+        if (i <= 0 )
+        {
+            Console.WriteLine();
+            break;
+        }
+    }
+}
+while (user != aiNumber);
+{
+    if (user == aiNumber) Console.WriteLine($"Победа!");
+    else Console.WriteLine($"Поражение :(\nЗагаданое число {aiNumber}");
+}
