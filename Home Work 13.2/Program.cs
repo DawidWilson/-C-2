c2e32dcc64822aa20ef3f9be2a382e3f4fcaec18
Владимир <Sovasol19@gmail.com>
2024-03-06 17:22:43 +0300

    Home Work 13.2

---
+++ Home Work 13.2/Program.cs
@@ -0,0 +1,17 @@
+﻿
+/*. Написать программу, которая будет генерировать случайные числа от
+1 до 10 и выводить их на экран до тех пор, пока не будет сгенерировано
+число 7.
+*/
+
+using System;
+
+Random random = new Random();
+int random1 = random.Next(1, 11);
+
+while (true)
+{
+    if (random1 != 7) Console.WriteLine($"Сгенирировано число {random1}");
+    if (random1 == 7) Console.WriteLine($"Сгенирировано {random1}");
+    break;
+}
