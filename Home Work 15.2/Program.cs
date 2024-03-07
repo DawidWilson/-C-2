00c45e0400787a6621556d0a8e3fb6da62b2b43f
Владимир <Sovasol19@gmail.com>
2024-03-08 00:39:23 +0300

    Home Work 15.2

---
+++ Home Work 15.2/Program.cs
@@ -0,0 +1,14 @@
+﻿int[] mas = new int[10];
+Random random = new Random();
+for (int i = 0; i < mas.Length; i++)
+{
+    mas[i] = random.Next(0, 11);
+    Console.Write(mas[i] + " ");
+    i++;
+}
+Console.WriteLine();
+foreach (int i in mas)
+{
+    if (i == 0) continue;
+    if (i % 2 == 0 ) Console.Write(i + " ");
+}
