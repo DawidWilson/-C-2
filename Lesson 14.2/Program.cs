31eb1923ef596399c9b69967aa5659a929f2f696
DawidWilson <86928107+DawidWilson@users.noreply.github.com>
2024-03-10 18:44:03 +0200

    Home Work 14.2

---
+++ Lesson 14.2/Program.cs
@@ -0,0 +1,28 @@
+﻿
+Random random = new Random();
+int temperature = 0;
+
+
+for (int i = 1; i > temperature; i++)
+{
+    temperature = random.Next(1, 101);
+
+    if (temperature < 90)
+    {
+        Console.Write($"Текущая температура:{temperature}");
+    }
+    else
+    {
+        Console.Write($"Критическая температура:{temperature}");
+        break;
+    }
+}
+
+
+
+
+
+
+
+
+
